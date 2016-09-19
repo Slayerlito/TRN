cc60854b6d37286efd996805b24c77c63d0a2398
Slayer <davidgarciaromero@gmail.com>
2016-09-19 17:22:40 +0200

    Añadidos los archivos login y registro. Eliminados Nuevo documento de texto en carpetas sql y documentacion Añadida la carpeta PHP con un archivo vacio para crear funciones y clases

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+//Include de los parametros de smarty
+require_once('setup.php'); 
+
+//Declaracion de la funcion de smarty
+$smarty = new Smarty_setup();
+
+//Include del header
+require_once('header.php');
+
+//Funcionalidad del login
+
+$smarty->display('login.tpl');
+
+//Include del footer
+require_once('footer.php');
+?>
